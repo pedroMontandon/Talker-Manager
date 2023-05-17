2e76c41d2cfb869f1003fcbde3d71729b05dab13
pedroMontandon <pedrobarbosa89@gmail.com>
2023-05-17 17:56:14 -0300

    5 feita

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const fs = require('fs').promises;
 const generateToken = require('./helpers/generateToken');
 const verifyEmail = require('./middlewares/verifyEmail');
 const verifyPassword = require('./middlewares/verifyPassword');
+const { verifyName, verifyAge, verifyTalk, verifyRate, verifyToken, verifyRateNumber } = require('./middlewares/verifyTalkerPost');
 
 const app = express();
 app.use(express.json());
@@ -31,6 +32,28 @@ app.get('/talker/:id', async (req, res) => {
 
   res.status(HTTP_OK_STATUS).json(talker);
 });
+
+app.post('/talker', verifyToken, verifyName, verifyAge, verifyTalk, verifyRate, verifyRateNumber, async (req, res) => {
+  const { name, age, talk } = req.body;
+  const talkers = await fs.readFile('./src/talker.json');
+  const talkersTreated = JSON.parse(talkers);
+
+  const id = (talkersTreated.at(-1).id + 1);
+
+  fs.writeFile('./src/talker.json', JSON.stringify([ ...talkersTreated, {
+    name, id, age, talk: { watchedAt: talk.watchedAt, rate: talk.rate }
+  }]), 'utf-8')
+
+  return res.status(201).json({
+    id,
+    name,
+    age,
+    talk: {
+      watchedAt: talk.watchedAt,
+      rate: talk.rate
+    }
+  })
+})
 
 app.post('/login', verifyEmail, verifyPassword, async (req, res) => {
   const token = generateToken(8);
