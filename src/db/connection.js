d3fc45c45cf863404f104435942570e26bed0f30
pedroMontandon <pedrobarbosa89@gmail.com>
2023-05-18 00:04:16 -0300

    Testando se passa no github com o arquivo .env

--- src/db/connection.js
+++ src/db/connection.js
@@ -1,16 +1,27 @@
 const mysql = require('mysql2/promise');
 
-const connection = mysql.createPool({
-   host: 'db',
-   port: 3306,
-   user: 'root',
-   password: 'password',
-   database: 'TalkerDB',
-   waitForConnections: true,
-   connectionLimit: 10,
-   queueLimit: 0, 
-});
+// const connection = mysql.createPool({
+//    host: 'db',
+//    port: 3306,
+//    user: 'root',
+//    password: 'password',
+//    database: 'TalkerDB',
+//    waitForConnections: true,
+//    connectionLimit: 10,
+//    queueLimit: 0, 
+// });
 
+const connection = mysql.createPool({
+    host: process.env.MYSQL_HOSTNAME,
+    port: process.env.MYSQL_PORT,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0, 
+ });
+ 
 // passa no local, mas não passa no github
 
 module.exports = connection;
